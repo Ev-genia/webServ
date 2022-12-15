ff47cd3f722ec77c53b288cb28c62471ac710246
Kostareva Eugenia <bzhenya2006@ya.ru>
2022-12-15 12:37:01 +0300

    add beauty

--- includes/Config.hpp
+++ includes/Config.hpp
@@ -6,7 +6,7 @@
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 13:22:52 by wcollen           #+#    #+#             */
-/*   Updated: 2022/12/14 17:32:41 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/12/15 12:36:19 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@
 class Config
 {
 private:
-	std::string _keyWords[11];
-	std::string	_contentString;
-	std::vector<Server> _serverTable;
+	std::string			_keyWords[11];
+	std::string			_contentString;
+	std::vector<Server>	_serverTable;
 	
 	std::pair<std::string, bool>		isKeyWord(std::string src, std::string keyWords[], int num);
 	std::pair<std::string, std::string> splitConfigParam(std::string src);
