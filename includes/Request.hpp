a647850bb9b835a3f0fb85a744c84a86aef16b59
Kostareva Eugenia <bzhenya2006@ya.ru>
2022-12-16 09:40:50 +0300

    Request::readFirstLine

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 16:56:20 by mlarra            #+#    #+#             */
-/*   Updated: 2022/12/12 17:16:34 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/12/15 17:36:06 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,25 @@
 
 # include "webserv.hpp"
 
+
+
 class Request
 {
 private:
-	int			_ret;
-	std::string	_metod;
+	int									_ret;
+	std::string							_metod;//TO DO: заменить на вставку строки в _requestMap
+	std::map<std::string, std::string>	_requestMap;
+	void								parseRequest(const std::string &);
+	void								readFirstLine(const std::string &);
+	std::string							nextLine(const std::string &, size_t &);
+	// std::string							&pop(std::string &);
+	void								pop(std::string &);
 public:
 	Request(const std::string);
 	~Request();
-	int		getRet() const;
-	void	setMethod(const std::string);
+	int									getRet() const;
+	void								setMethod(const std::string);
+	// std::map<std::string, std::string>	&getRequestMap();
 };
 
 
--- src/Request.cpp
+++ src/Request.cpp
@@ -6,17 +6,62 @@
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 16:56:02 by mlarra            #+#    #+#             */
-/*   Updated: 2022/12/13 16:29:34 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/12/15 17:39:11 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Request.hpp"
 
-Request::Request(const std::string str): _ret(200)
+Request::Request(const std::string str)
 {
+	parseRequest(str);
 	if (_ret != 200)
 		std::cerr << "Error init request" << std::endl;
-	(void)str;
+}
+
+/*
+GET / HTTP/1.1\nHost: developer.mozilla.org\nAccept-Language: fr\r\n\r\n<body>
+*/
+
+// std::string	&Request::pop(std::string &str)
+void	Request::pop(std::string &str)
+{
+	if (str.size())
+		str.resize(str.size() - 1);
+	// return (str);
+}
+
+std::string	Request::nextLine(const std::string &str, size_t &i)
+{
+	std::string	ret;
+	size_t		j;
+
+	if (i == std::string::npos)
+		return ("");
+	j = str.find_first_of('\n', i);
+	ret = str.substr(i, j - i);
+	if (ret[ret.size() - 1] == '\r')
+		pop(ret);
+	if (j == std::string::npos)
+		i = j;
+	else
+		i = j + 1;
+	return (ret);
+}
+
+void	Request::readFirstLine(const std::string &line)
+{
+	
+}
+
+void	Request::parseRequest(const std::string &enterRequest)
+{
+	size_t	poz = 0;
+	
+	readFirstLine(nextLine(enterRequest, poz));
+
+
+	_ret = 200;
 }
 
 Request::~Request()
