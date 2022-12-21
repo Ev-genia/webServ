383272442774ca798b77812f70ee267a3c6ac01b
Kostareva Eugenia <bzhenya2006@ya.ru>
2022-12-21 12:00:09 +0300

    request done

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 16:56:20 by mlarra            #+#    #+#             */
-/*   Updated: 2022/12/20 17:27:16 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/12/21 11:46:13 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,35 +23,38 @@ private:
 	std::string							_method;
 	std::string							_version;
 	std::string							_path;
-	std::map<std::string, std::string>	_requestMap;//need add initRequestMap
+	std::map<std::string, std::string>	_requestMap;
 	std::string							_body;
+	std::string							_query;
 	std::map<std::string, std::string>	_envForCgi;
 	int									_ret;
 	static std::vector<std::string>		availableMethods;
 	static std::vector<std::string>		initMethods();
-	void								resetRequestMap();
+	void								initRequestMap();
 	void								parseRequest(const std::string &);
 	void								readFirstLine(const std::string &);
 	void								readPath(const std::string &, size_t);
 	void								readVersion(const std::string &, size_t);
 	void								checkMethod();
 	std::string							nextLine(const std::string &, size_t &);
-	// std::string							&pop(std::string &);
-	void								pop(std::string &);
 	std::string							readKey(const std::string &);
 	std::string							readValue(const std::string &);
-	std::string							&strip(std::string &, char);
 	// void								setLang();
-	std::vector<std::string>			split(const std::string &, char);
 	void								setBody(const std::string &);
-
+	void								findQuery();
 
 public:
 	Request(const std::string);
 	~Request();
-	int									getRet() const;
-	void								setMethod(const std::string);
-	std::map<std::string, std::string>	&getRequestMap();
+	const std::string							&getMethod() const;
+	const std::string							&getVersion() const;
+	const std::string							&getPath() const;
+	const std::map<std::string, std::string>	&getRequestMap() const;
+	const std::string							&getBody() const;
+	const std::string							&getQuery() const;
+	const std::map<std::string, std::string>	&getEnvForCgi() const;
+	int											getRet() const;
+	void										setMethod(const std::string);
 };
 
 
--- includes/utils.hpp
+++ includes/utils.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Utils.hpp                                          :+:      :+:    :+:   */
+/*   utils.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 13:35:08 by mlarra            #+#    #+#             */
-/*   Updated: 2022/12/05 13:39:35 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/12/21 11:09:45 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 
 # include "webserv.hpp"
 
-void	exitError(std::string str);
+void						exitError(std::string str);
+std::string					&strip(std::string &, char);
+void						pop(std::string &);
+std::vector<std::string>	split(const std::string &, char);
+
 
 #endif
