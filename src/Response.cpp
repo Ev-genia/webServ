a58e17b3ccc32f74ea0c75fe0eb3315a2f135fc0
AlinaNadezhdina <nadezdinaalina99@gmail.com>
2022-12-21 16:25:26 +0300

    add Response constructor

--- src/Response.cpp
+++ src/Response.cpp
@@ -3,11 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   Response.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
+/*   By: wcollen <wcollen@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 15:26:03 by mlarra            #+#    #+#             */
-/*   Updated: 2022/12/13 15:26:04 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/12/21 15:59:40 by wcollen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Response.hpp"
+
+Response::Response(strStrMap requestMap, Server &server, Client *client)
+	:_requestMap(requestMap), _server(server), _client(client){}
+
+Response::~Response(){}
+
