8d1149a8ca0ba219b650cc254b7b938de178781c
AlinaNadezhdina <nadezdinaalina99@gmail.com>
2023-01-25 14:07:54 +0300

    edit RC continue

--- includes/ResponseConfig.hpp
+++ includes/ResponseConfig.hpp
@@ -6,7 +6,7 @@
 /*   By: wcollen <wcollen@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 16:29:05 by mlarra            #+#    #+#             */
-/*   Updated: 2023/01/13 15:46:37 by wcollen          ###   ########.fr       */
+/*   Updated: 2023/01/25 14:07:30 by wcollen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ private:
 	std::string		_contentLocation;
 	std::string		_path;
 	std::string		_error_page;// ????
-	unsigned long	body_size; // max size for the client body, defaults to 8 000??
+	unsigned long	_body_size; // max size for the client body, defaults to 8 000??
 	std::string		_lang;
 	std::string		_method_allowed;
 	std::string		_autoindex;
--- src/ResponseConfig.cpp
+++ src/ResponseConfig.cpp
@@ -6,7 +6,7 @@
 /*   By: wcollen <wcollen@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 16:23:11 by mlarra            #+#    #+#             */
-/*   Updated: 2023/01/13 15:48:54 by wcollen          ###   ########.fr       */
+/*   Updated: 2023/01/25 14:07:25 by wcollen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,23 @@
 ResponseConfig::ResponseConfig(Server &server, Request &request): _server(server), _request(request)
 {
 	std::string	ret;
+	std::vector<Location> locations = server.getLocations();
+	std::vector<Location>::iterator it;
+	// for (it = locations.begin(); it < locations.end(); it++)
+	// {
+	// 	if (it->getPath() == request.getUri())
+	// 	{
+	// 		_body_size = it->getLocationMap()["body_size"];
+	// 	}
+	// }
+	
 	//this->body_size = server.getLocations()//в каком Location сервера взять body_size???
 	_error_page = server.getParams()["error_page"];
 	_path = removeSlashes(ret);
 	_method_allowed = server.getParams()["method_allowed"];
 	_autoindex = server.getParams()["autoindex"];
+
+
 
 }
 
